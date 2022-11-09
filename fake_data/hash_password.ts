735db0f0d58dbd91346b2d29dbe8e84b98ab8a0e
Howard <howardshen2306@gmail.com>
2022-11-09 20:51:26 +0800

    feat: remove log

--- fake_data/hash_password.ts
+++ fake_data/hash_password.ts
@@ -6,8 +6,6 @@ async function hashPassword() {
   try {
     const hash1 = await argon2.hash('123456');
     const hash2 = await argon2.hash('654321');
-    console.log(hash1);
-    console.log(hash2);
   } catch (error) {
     console.log(error);
   }
